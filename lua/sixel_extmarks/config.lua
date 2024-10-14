a431202ebf741953743882d13db6847adb28243a
queue-miscreant <cubebert549@gmail.com>
2024-10-14 04:21:41 -0500

    improve config loading

--- lua/sixel_extmarks/config.lua
+++ lua/sixel_extmarks/config.lua
@@ -6,7 +6,7 @@ local default_config = {
   -- System ioctl value for TIOCGWINSZ
   -- This is the default value in Linux (and other kernels),
   -- but can also be derived from the Python installation via ioctl_magic
-  TIOCGWINSZ =  0x5413,
+  TIOCGWINSZ = 0x5413,
   -- Can ascend pseudoterminals
   parent_tty_magic = true,
 
@@ -24,24 +24,15 @@ local default_config = {
   slow_insert = false,
 }
 
-local config = default_config
+-- Configuration settings which can be partially updated from user config
+local table_configs = {}
+local GLOBAL_PREFIX = "image_extmarks_"
 
-function config.load_globals(opts)
-  -- Load the new options or global variables
-  for option, default_value in pairs(default_config) do
-    local global_value = vim.g["image_extmarks_" .. option]
-    local lazy_value = opts[option]
-    if global_value ~= nil then
-      config[option] = global_value
-    elseif lazy_value ~= nil then
-      config[option] = lazy_value
-    end
+-- Start with defaults
+local config = vim.deepcopy(default_config) or default_config
 
-    if config[option] == nil then
-      config[option] = default_value
-    end
-  end
-
+-- Additional updates to config
+local function update_config()
   -- Unset this so config can fetch it later
   if config.ioctl_magic then
     config.TIOCGWINSZ = nil
@@ -62,6 +53,52 @@ function config.load_globals(opts)
       vim.log.levels.WARN
     )
   end
+end
+
+-- Load options from global variables and argument options
+function config.load_globals(opts)
+  -- Load the new options or global variables
+  for option, default_value in pairs(default_config) do
+    local global_value = vim.g[GLOBAL_PREFIX .. option]
+    -- Convert Vim global from truthy number to boolean
+    if type(global_value) == "number" and type(default_value) == "boolean" then
+      global_value = global_value ~= 0
+    end
+    local lazy_value = opts[option]
+
+    if global_value ~= nil then
+      config[option] = global_value
+    -- Only set lazy-configured options when table_configs
+    elseif lazy_value ~= nil and table_configs[option] == nil then
+      config[option] = lazy_value
+    end
+
+    if config[option] == nil then
+      config[option] = vim.deepcopy(default_value)
+    end
+  end
+
+  -- Load tableized options
+  for _, option in ipairs(table_configs) do
+    ---@diagnostic disable-next-line
+    for suboption, _ in pairs(default_config[option] or {}) do
+      local global_value = vim.g[GLOBAL_PREFIX .. option .. "_" .. suboption]
+      -- Convert Vim global from truthy number to boolean
+      if type(global_value) == "number" and type(default_config[option][suboption]) == "boolean" then
+        global_value = global_value ~= 0
+      end
+      local lazy_value = (opts[option] or {})[suboption]
+
+      if global_value ~= nil then
+        config[option][suboption] = global_value
+      end
+      if lazy_value ~= nil then
+        config[option][suboption] = lazy_value
+      end
+    end
+  end
+
+  update_config()
 end
 
 return config
